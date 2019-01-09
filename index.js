a8e1414dd424b30e12523ab8eb9afad3498d539a
unknown <tpy-sta-02@moj.ad>
2019-01-09 12:18:45 +0800

    safety precaustions

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var rightAudio = new Audio('right.mp3');
-var wrongAudio = new Audio('wrong.mp3');
+var rightAudio = document.getElementById('rightAudio');
+var wrongAudio = document.getElementById('wrongAudio');
 
 var rightButton = document.getElementById('right');
 var wrongButton = document.getElementById('wrong');
@@ -15,23 +15,21 @@ function setButtonsState(disabled) {
 }
 
 rightButton.addEventListener('click', function(){
-    console.log("clicked right button");
     rightAudio.play();
     setButtonsState(true);
 })
 
 wrongButton.addEventListener('click', function(){
-    console.log("clicked wrong button");
     wrongAudio.play();
     setButtonsState(true);
 })
 
-rightButton.addEventListener('ended', function(){
+rightAudio.addEventListener('ended', function(){
     console.log("right audio playback ended");
     setButtonsState(false);
 })
 
-wrongButton.addEventListener('ended', function(){
+wrongAudio.addEventListener('ended', function(){
     console.log("wrong audio playback ended");
     setButtonsState(false);
 })
